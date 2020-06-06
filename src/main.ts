d51f321a1ef33996be0fc41ca6d5a044472d76e6
Thiago Moura <thmoura14@gmail.com>
2020-06-06 13:22:17 -0300

    feat: Implemented get users router

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { Server } from './server/Server'
+import { Server } from './server/server'
+import { usersRouter } from './users/users.router'
 
 const server = new Server()
 
-server.bootstrap()
+server.bootstrap([usersRouter])
   .then(server => {
     console.log(`Server is listening on: ${server.application.address()}`)
   })
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,11 +1,12 @@
 import * as restify from 'restify'
 
 import { environment } from '../common/environment'
+import { Router } from '../common/router'
 
 export class Server {
   application: restify.Server
 
-  initRoutes(): Promise<any> {
+  initRoutes(routers: Router[]): Promise<any> {
     return new Promise((resolve, reject) => {
       try {
         this.application = restify.createServer({
@@ -15,10 +16,8 @@ export class Server {
 
         this.application.use(restify.plugins.queryParser())
 
-        this.application.get('/hello', (req, res, next) => {
-          res.json({ message: 'Hello World' })
-          return next()
-        })
+        // Routes
+        routers.forEach(router => router.applyRoutes(this.application))
 
         this.application.listen(environment.server.port, () => {
           resolve(this.application)
@@ -29,7 +28,7 @@ export class Server {
     })
   }
 
-  bootstrap(): Promise<Server> {
-    return this.initRoutes().then(() => this)
+  bootstrap(routers: Router[] = []): Promise<Server> {
+    return this.initRoutes(routers).then(() => this)
   }
 }
